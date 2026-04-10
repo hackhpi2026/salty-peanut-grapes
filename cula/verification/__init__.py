92929c2a6430e85fbdb4a84e1e74ad59843799a9
ilag11 <ilag11@mi.fu-berlin.de>
2026-04-10 22:20:39 +0200

    add steps fetch and normalization

---
+++ cula/verification/__init__.py
@@ -0,0 +1,4 @@
+from cula.verification.fetch import FetchResult, fetch_sink_data
+from cula.verification.normalize import NormalizedContext, normalize
+
+__all__ = ["FetchResult", "fetch_sink_data", "NormalizedContext", "normalize"]
